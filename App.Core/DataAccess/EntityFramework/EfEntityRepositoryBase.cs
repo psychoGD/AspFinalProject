e5983079ccffb6841995972f4d21cbdfb021c9d4
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-06-17 15:58:31 +0400

    monolit and asset is working

---
+++ App.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,80 @@
+﻿using App.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace App.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext>
+        : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        
+        public void Add(TEntity entity)
+        {
+            using (var context=new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State= EntityState.Added;   
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context=new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public virtual List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+
+            using (var context = new TContext())
+            {
+
+        //                public Main Main { get; set; }
+        //public int EngineId { get; set; }
+        //public Engine Engine { get; set; }
+        //public int StatusId { get; set; }
+        //public Status Status { get; set; }
+        //public int FinanceId { get; set; }
+        //public Finance Finance { get; set; }
+        //public int OwnerId { get; set; }
+        //public User Owner { get; set; }
+        //public List<Image> Images { get; set; }
+        //public List<Post> Posts { get; set; }
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updateEntity = context.Entry(entity);
+                updateEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
