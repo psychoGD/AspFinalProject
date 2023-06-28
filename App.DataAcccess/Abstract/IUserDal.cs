b81a940398a8b44d1b909d8f4fb53bd56d18714a
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-06-28 19:51:00 +0400

    app data access is updated

---
+++ App.DataAcccess/Abstract/IUserDal.cs
@@ -0,0 +1,15 @@
+﻿using App.Core.DataAccess;
+using App.Core.Entities;
+using App.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DataAcccess.Abstract
+{
+    public interface IUserDal:IEntityRepository<User>
+    {
+    }
+}
