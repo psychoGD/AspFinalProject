9d0f50bd06501719f30366a3cb613825e3380bd2
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-06-28 20:01:46 +0400

    All Apps Updated To Core

--- App.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ App.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,9 +1,11 @@
 ﻿using App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace App.Core.DataAccess.EntityFramework
 {
@@ -12,13 +14,13 @@ namespace App.Core.DataAccess.EntityFramework
         where TEntity : class, IEntity, new()
         where TContext : DbContext, new()
     {
-        
+
         public void Add(TEntity entity)
         {
-            using (var context=new TContext())
+            using (var context = new TContext())
             {
                 var addedEntity = context.Entry(entity);
-                addedEntity.State= EntityState.Added;   
+                addedEntity.State = EntityState.Added;
                 context.SaveChanges();
             }
         }
@@ -35,7 +37,7 @@ namespace App.Core.DataAccess.EntityFramework
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
         {
-            using (var context=new TContext())
+            using (var context = new TContext())
             {
                 return context.Set<TEntity>().SingleOrDefault(filter);
             }
@@ -47,17 +49,17 @@ namespace App.Core.DataAccess.EntityFramework
             using (var context = new TContext())
             {
 
-        //                public Main Main { get; set; }
-        //public int EngineId { get; set; }
-        //public Engine Engine { get; set; }
-        //public int StatusId { get; set; }
-        //public Status Status { get; set; }
-        //public int FinanceId { get; set; }
-        //public Finance Finance { get; set; }
-        //public int OwnerId { get; set; }
-        //public User Owner { get; set; }
-        //public List<Image> Images { get; set; }
-        //public List<Post> Posts { get; set; }
+                //                public Main Main { get; set; }
+                //public int EngineId { get; set; }
+                //public Engine Engine { get; set; }
+                //public int StatusId { get; set; }
+                //public Status Status { get; set; }
+                //public int FinanceId { get; set; }
+                //public Finance Finance { get; set; }
+                //public int OwnerId { get; set; }
+                //public User Owner { get; set; }
+                //public List<Image> Images { get; set; }
+                //public List<Post> Posts { get; set; }
                 return filter == null
                     ? context.Set<TEntity>().ToList()
                     : context.Set<TEntity>().Where(filter).ToList();
--- App.Core/DataAccess/IEntityRepository.cs
+++ App.Core/DataAccess/IEntityRepository.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace App.Core.DataAccess
 {
-    public interface IEntityRepository<T> where T:class,IEntity,new()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        T Get(Expression<Func<T, bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter = null);
         List<T> GetList(Expression<Func<T, bool>> filter = null);
         void Add(T entity);
-        void Update(T entity);  
-        void Delete(T entity);  
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
